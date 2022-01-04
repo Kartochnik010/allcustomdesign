92f75c9b0da9deb76a5b4b86b804a7780e61182c
Kartochnik010 <78559811+Kartochnik010@users.noreply.github.com>
2022-01-04 21:04:29 +0600

    first upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function redirect1() {
+    window.location = "https://www.youtube.com/watch?v=tRpoI6vkqLs&t=806s";
+}
+function redirect2() {
+    window.open("https://www.youtube.com/watch?v=tRpoI6vkqLs&t=806s", "_blank");
+}
